f601b392ec9cfb61929b0bfab32d3371a280190a
kir563 <k563ke70@gmail.com>
2023-04-19 16:38:09 +0700

    added createGetter()

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const keys = path.split('.')
+    
+    return function getValue(object, current = 0) {
+        const value = object[keys[current]]
 
+        if (current === (keys.length - 1) || !value) {
+            return value
+        }
+
+        return getValue(value, current + 1)
+    }
 }
